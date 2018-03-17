70cd6a5869d52b75bedbfb9ff3c13c56e94ed5e8
imar26 <aadeshranderia26@gmail.com>
2018-03-17 18:08:28 -0400

    ui done using scss

--- TodoApp/src/app/app.module.ts
+++ TodoApp/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
