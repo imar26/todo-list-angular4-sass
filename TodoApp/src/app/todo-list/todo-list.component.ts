550e52c4bfe1a4512319fb88b68d6e675d93e38c
Bhumika <khatri.bh@husky.neu.edu>
2018-03-17 20:48:08 -0400

    Displaying JSON Data

--- TodoApp/src/app/todo-list/todo-list.component.ts
+++ TodoApp/src/app/todo-list/todo-list.component.ts
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
-
+  mainHeader: string = "Welcome to TechCrat's Todo List";
+  leftTitle: string = "List of Todo Items";
+  rightTitle: string = "Add/Update Todo Item";
+  todoLists: Array<any> = [
+    {
+        "title": "Cooking",
+        "todoItem": "Cook food for 8 people",
+        "author": "Aadesh",
+        "date": "2018-02-12"
+    },
+    {
+        "title": "Grocery",
+        "todoItem": "Get Milk and Bread",
+        "author": "Bhumika",
+        "date": "2018-02-13"
+    },
+    {
+        "title": "Cleaning",
+        "todoItem": "Kitchen area and bathtub",
+        "author": "Aadesh",
+        "date": "2018-02-14"
+    },
+    {
+        "title": "Laundry",
+        "todoItem": "Take the bedsheets and curtains",
+        "author": "Bhumika",
+        "date": "2018-02-15"
+    },
+    {
+        "title": "Bills",
+        "todoItem": "Pay the electricity and Wi-fi bills",
+        "author": "Bhumika",
+        "date": "2018-02-16"
+    }
+];
+authorList : Array<any> = ['Aadesh', 'Bhumika', 'Rohit', 'Meven', 'Li'];
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
