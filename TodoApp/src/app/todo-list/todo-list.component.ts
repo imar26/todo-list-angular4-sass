0a028945941ba3afae9a8880be9fe1beb0bd100f
imar26 <aadeshranderia26@gmail.com>
2018-03-19 19:32:34 -0400

    Angular Validations / Success Message

--- TodoApp/src/app/todo-list/todo-list.component.ts
+++ TodoApp/src/app/todo-list/todo-list.component.ts
@@ -12,11 +12,15 @@ export class TodoListComponent implements OnInit {
     @ViewChild('f') todoForm: NgForm;
 
     //properties
+    clicked: string;
     id: any;
     title: string;
     todoItem: string;
     author: string;
     date: any;
+    addItem: boolean;
+    updateItem: boolean;
+    success: string;
     mainHeader: string = "Welcome to TechCrat's Todo List";
     leftTitle: string = "List of Todo Items";
     rightTitle: string = "Add/Update Todo Item";    
@@ -31,25 +35,44 @@ export class TodoListComponent implements OnInit {
         for(let item of this.todoLists) {
             item["id"] = Math.floor(Math.random() * 100000000);
         }
+        this.todoForm.value.author = "Aadesh";
     }
 
     // Function called on add/update TODO Item
     saveTodoList() {
-        this.id = Math.floor(Math.random() * 100000000);
+        if(this.todoForm.value.id) {
+            this.id = this.todoForm.value.id;
+        } else {
+            this.id = Math.floor(Math.random() * 100000000);
+        }
         this.title = this.todoForm.value.title;
         this.todoItem = this.todoForm.value.todoItem;
         this.author = this.todoForm.value.author;
         this.date = this.todoForm.value.date;
-
         let todo = {
             "id": this.id,
             "title": this.title,
             "todoItem": this.todoItem,
             "author": this.author,
             "date": this.date
-
         }
         this.todoLists.push(todo);
+        this.todoForm.reset();
+        if(this.todoForm.value.id) {
+            this.updateItem = true;
+            this.success = "TODO Item update successfully.";
+            let timeoutId = setTimeout(() => {
+                this.updateItem = false;
+            }, 2500);
+            clearTimeout(timeoutId);
+        } else {
+            this.addItem = true;
+            this.success = "TODO Item added successfully.";
+            let timeoutId = setTimeout(() => {
+                this.addItem = false;
+            }, 2500);
+            clearTimeout(timeoutId);
+        }
     }
 
     // Delete TODO Item
